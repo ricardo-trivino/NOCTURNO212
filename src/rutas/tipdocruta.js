a8fa5430a6853b955ee7b6e0077f1701a7d30d03
ricardo-trivino <ricardo.trivinob@gmail.com>
2021-11-10 10:37:26 -0500

    First commit BackEnd

---
+++ src/rutas/tipdocruta.js
@@ -0,0 +1,102 @@
+//instanciamos las Librerias 
+var express = require('express');
+var router = express.Router();
+
+//obtenemos el modelo TipDocModel con toda la funcionalidad
+var TipDocModel = require('../modelos/tipdocmodel');
+
+//creamos el ruteo de la clase
+module.exports = function () {
+
+    //---------------------------------------------------------------
+    //Muestra el método CRUL Listar que muestra todos los tipos de documentos
+    router.get("/", function (req, res) {
+        TipDocModel.getTipDocs(function (error, data) {
+            res.status(200).json(data);
+            console.log("Este es el dato: " + data[3].tipo_documento + " - " + data[3].iniciales_tip_doc);
+        });
+    });
+
+    //---------------------------------------------------------------
+    //Muestra el método CRUL read(leer), que muestra el tipo de documento solicitado
+    router.get("/:id", function (req, res) {
+        var id = req.params.id;
+
+        //solo actualizamos si la id es un número
+        if (!isNaN(id)) {
+            TipDocModel.getTipDoc(id, function (error, data) {
+                //si el tipo de documento existe lo mostramos en formato json
+                if (typeof data !== 'undefined' && data.length > 0) {
+                    res.status(200).json(data);
+                }
+                //en otro caso mostramos una respuesta conforme no existe
+                else {
+                    res.json(404,
+                        {
+                            "msg": "Registro no Existe"
+                        });
+                }
+            });
+        }
+        else //si hay algún error
+        {
+            res.status(500).json({ "msg": "error" });
+        }
+    });
+
+    //---------------------------------------------------------------
+    //Muestra y captura los datos del método CRUL crear, usando el verbo post
+    router.post("/", function (req, res) {
+        //creamos un objeto Json con los datos del tipo de documento
+        var TipDocData =
+        {
+            id_tip_doc: null,
+            tipo_documento: req.body.tipo_documento,
+            iniciales_tip_doc: req.body.iniciales_tip_doc,
+        };
+        //SELECT `id_tip_doc`, `tipo_documento`, `iniciales_tip_doc` FROM `ct_tipos_documentos`
+
+        //usamos la funcion para insertar
+        TipDocModel.insertTipDoc(TipDocData, function (error, data) {
+            //se muestra el mensaje correspondiente
+            if (data) {
+                res.status(200).json(data);
+            }
+            else {
+                res.status(500).send({ error: "boo:(" });
+            }
+        });
+    });
+
+    //---------------------------------------------------------------
+    //Muestra y captura los datos para el método CRUL update (actualizar), usando el verbo put
+    router.put("/", function (req, res) {
+        //almacenamos los datos de la petición en un objeto
+        //console.log(" 38");
+        var TipDocData =
+        {
+            id_tip_doc: req.body.id_tip_doc,
+            tipo_documento: req.body.tipo_documento,
+            iniciales_tip_doc: req.body.iniciales_tip_doc,
+        };
+
+
+        //usamos la funcion para actualizar
+        TipDocModel.updateTipDoc(TipDocData, function (error, data) {
+            //se muestra el mensaje correspondiente
+            if (data && data.msg) {
+                res.status(200).json(data);
+            }
+            else {
+                res.status(500).send(
+                    {
+                        error: "boo:("
+                    });
+            }
+        });
+    });
+
+
+    //exportamos el objeto para tenerlo disponible en EL APP
+    return router;
+}
