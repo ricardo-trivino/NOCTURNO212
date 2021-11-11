6f156c7bbcadc9a6ea19d5fafcef643970829f0a
ricardo-trivino <ricardo.trivinob@gmail.com>
2021-11-10 23:43:47 -0500

    All functionality and styles complete

--- src/app/servicio-nocturno.service.ts
+++ src/app/servicio-nocturno.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { map, catchError, tap } from 'rxjs/operators';
 
 const httpOptions =
 {
@@ -14,27 +13,7 @@ const httpOptions =
 export class ServicioNocturnoService {
 
   private Url: string = 'http://localhost:3000';
-
   constructor(private http: HttpClient) { }
-
-  private extractData(res: Response) {
-
-    let body = JSON.parse('' + res);
-    return body || {};
-  }
-
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      console.log(`${operation} failed: ${error.message}`);
-      return of(result as T)
-
-    };
-  }
-
-  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  // SERVICIO CRUD DE TIPOS DE DOCUMENTOS
-  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
 
   // Método Listar de los Tipos de documentos 
   getTipDocs(): Observable<any> {
@@ -43,7 +22,6 @@ export class ServicioNocturnoService {
 
   }
 
-  //-------------------------------------------------------------
   // Método mostrar un solo Tipo de documento
   getTipDoc(id: any): Observable<any> {
 
@@ -51,57 +29,22 @@ export class ServicioNocturnoService {
 
   }
 
-  /*
-    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    // SERVICIO CRUD DE TIPOS DE DOCUMENTOS
-    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  
-    // Método Listar de los Tipos de documentos
-    getTipDocs(): Observable<any> {
-  
-      //console.log("estamos aqui  " + this.Url+ "/marca", httpOptions);
-  
-      return this.http.get(this.Url + "/tipdoc", httpOptions).pipe(
-        map(this.extractData)
-      );
-    }
-  
-    //-------------------------------------------------------------
-    // Método mostrar un solo Tipo de documento
-  
-    getTipDoc(id): Observable<any> {
-  
-      return this.http.get(this.Url + "/tipdoc" + id, httpOptions).pipe(
-        map(this.extractData));
-    }
-  
-    //-------------------------------------------------------------
-    // Método para insertar un nuevo Tipo de documento
-  
-    async insertTipDoc(TipDocD): Promise<any> {
-  
-      //console.log(TipDocD, this.Url+"/tipdoc")
-  
-      return new Promise((resolve, reject) => {
-        this.http.post(this.Url + "/tipdoc", TipDocD, httpOptions).toPromise()
-      });
-    }
-  
-    //-------------------------------------------------------------
-    // Método para modificar un  Tipo de documento
-  
-    async updateTipDoc(cadena): Promise<any> {
-  
-      //console.log("33 " + cadena)
-      //console.log("tales 60  " + cadena.id_tip_doc + " - " + cadena.tipo_documento+ " - " +  cadena.iniciales_tip_doc, this.Url + "/tipdoc")
-  
-  
-      return new Promise((resolve, reject) => {
-        this.http.put(this.Url + "/tipdoc", cadena, httpOptions).toPromise()
-      });
-    }
-  
-    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  
-  */
+  // Método para insertar un nuevo Tipo de documento 
+  async insertTipDoc(TipDocD: any): Promise<any> {
+
+    return new Promise((resolve, reject) => {
+      this.http.post(this.Url + "/tipdoc", TipDocD, httpOptions).toPromise()
+    });
+
+  }
+
+  // Método para modificar un  Tipo de documento
+  async updateTipDoc(cadena: any): Promise<any> {
+
+    return new Promise((resolve, reject) => {
+      this.http.put(this.Url + "/tipdoc", cadena, httpOptions).toPromise()
+    });
+
+  }
+
 }
--- src/app/tipos-doc/tipos-doc.component.ts
+++ src/app/tipos-doc/tipos-doc.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Http } from '@angular/http';
 
 import { ServicioNocturnoService } from '../servicio-nocturno.service';
 
@@ -12,9 +11,9 @@ import { ServicioNocturnoService } from '../servicio-nocturno.service';
 })
 export class TiposDocComponent implements OnInit {
 
-  TipDocs: any = [];              //Lista de Tipos de Documentos
-  TituloTipDocs = "";             //Titulo Lista de Tipos de Documentos
-  TablaTipDocs: any = [];        //Encabezados tabla Lista de Tipos de Documentos
+  TipDocs: any = []; //Lista de Tipos de Documentos
+  TituloTipDocs = ""; //Titulo Lista de Tipos de Documentos
+  TablaTipDocs: any = []; //Encabezados tabla Lista de Tipos de Documentos
 
   TituloTipDoc = ""; //Titulo del tipo de doc buscado
   MiTipDoc: any = []; //Tipo de documento buscado
@@ -25,16 +24,29 @@ export class TiposDocComponent implements OnInit {
   controlLista = 1;  //Control para limpiar lista
   BuscarEvalor = 1; //Control para carga el valor a buscar
 
-  //************************************************************************************
+  TituloTipDocEdit = ""; //Titulo de Tipo de Documento a Editar
+  MiTipDocE: any = []; //Tipo de Documento a Editar
+  comboEditarTipDoc: any = []; //Combo Editar Tipo de Documento
+
   //Form group 
   ListaTiposDocum = new FormGroup(
     {
 
     });
-
   filtrarTipDoc = new FormGroup(
     {
       combofiltro: new FormControl()
+    });
+  InsertarGTipDoc = new FormGroup(
+    {
+      textTipDoc: new FormControl(),
+      textIniTipDoc: new FormControl()
+    });
+  ActualizarATipDoc = new FormGroup(
+    {
+      BuscarIdTipDoc: new FormControl(),
+      textnuevotipdoc: new FormControl(),
+      textnuevoinicialestipdoc: new FormControl()
     });
 
   constructor
@@ -44,41 +56,31 @@ export class TiposDocComponent implements OnInit {
       Router: Router
     ) { }
 
-  //---------------------------------------------------------------------------------------
-  //Consultar todos los tipos documentos
+  //Consultar todos los tipos de documentos
   public consultaTipoDocumentos(op: any) {
+
     if (this.controlLista == 1) {
-      //console.log("component")
       this.servi.getTipDocs().subscribe((data: any) => {
-        //console.error(" El listado 2 " );
         if (op == 1) {
           let dat = data;
-
-          this.TipDocs = data; //JSON.parse(data);
+          this.TipDocs = data;
           this.TituloTipDocs = "LISTA DE TIPOS DE DOCUMENTOS";
           this.TablaTipDocs[0] = "Indicador";
           this.TablaTipDocs[1] = "Denominación";
           this.TablaTipDocs[2] = "Iniciales";
-          //console.error(" El listado 3 " + this.TipDocs);
         }
         else if (op == 2) {
-          this.comboListaTipDoc = data; //JSON.parse(data);
+          this.comboListaTipDoc = data;
           this.MiTipDoc = null;
-          this.TituloTipDoc = "TIPO DE DOCUMENTO SELECCIONADO";
-          this.TabBusTipDocs[0] = "Indicador";
-          this.TabBusTipDocs[1] = "Denominación";
-          this.TabBusTipDocs[2] = "Iniciales";
-          //console.error(" El listado 4 ");
+          this.TituloTipDoc = "";
+          this.TabBusTipDocs[0] = "";
+          this.TabBusTipDocs[1] = "";
         }
-        else if (op == 3) {/*
-        this.comboEditarTipDoc = JSON.parse(data);
-        this.MiTipDocE = null;
-        this.TituloTipDocEdit = "";
-        // this.ActualizarATipDoc.removeControl("textnuevotipdoc");
-        // this.ActualizarATipDoc.removeControl("textnuevoinicialestipdoc");
-        console.error(" El listado 5 ");*/
+        else if (op == 3) {
+          this.comboEditarTipDoc = data;
+          this.MiTipDocE = null;
+          this.TituloTipDocEdit = "";
         }
-
       },
         error => { console.error(error + " ") });
     }
@@ -90,30 +92,90 @@ export class TiposDocComponent implements OnInit {
       this.TablaTipDocs[2] = "";
       this.controlLista = 1;
     }
+
   }
 
-  //---------------------------------------------------------------------------------------------
   //Limpiar la lista
-
   public LimpiarLista() {
+
     this.controlLista = 0;
+
   }
 
-  //---------------------------------------------------------------------------------------------
-  //Busca tipo de documento por su id
+  //Buscar un tipo de documento por su id
   public buscarTipDoc() {
+
     var filtrovalor = this.filtrarTipDoc.getRawValue()['combofiltro'];
     this.servi.getTipDoc('/' + filtrovalor).subscribe((data: {}) => {
-      this.MiTipDoc = data;//"TIPO DE DOCUMENTO SELECCIONADO";
-      /*this.TabBusTipDocs[0] = "Indicador";
+      this.MiTipDoc = data;
+      this.TituloTipDoc = "TIPO DE DOCUMENTO SELECCIONADO";
+      this.TabBusTipDocs[0] = "Indicador";
       this.TabBusTipDocs[1] = "Denominación";
-      this.TabBusTipDocs[2] = "Iniciales";*/
     },
       error => { console.log(error) });
+
   }
-  //----------------------------------------------------------------------------------------------
+
+  //Insertar un tipo de documento
+  public InsertarTipDoc() {
+
+    var datosvalo2 = this.InsertarGTipDoc.getRawValue()['textTipDoc'];
+    var datosvalo1 = this.InsertarGTipDoc.getRawValue()['textIniTipDoc'];
+    var cadena = { "tipo_documento": datosvalo2, "iniciales_tip_doc": datosvalo1 };
+
+    this.servi.insertTipDoc(cadena).then
+      (res => {
+        console.log(res)
+      }
+      ).catch(err => {
+        console.log(err)
+      });
+    this.InsertarGTipDoc.reset();
+
+  }
+
+  //Buscar un tipo de documento su id para editarlo
+  buscarEditarTipDoc() {
+
+    if (this.BuscarEvalor != 0) {
+      this.BuscarEvalor = this.ActualizarATipDoc.getRawValue()['BuscarIdTipDoc'];
+      console.error(" dos el filtro " + this.BuscarEvalor);
+    }
+    console.error(" tres el filtro " + this.BuscarEvalor);
+
+    this.servi.getTipDoc('/' + this.BuscarEvalor).subscribe((data: {}) => {
+
+      this.MiTipDocE = data;
+      this.TituloTipDocEdit = "TIPO DE DOCUMENTO A EDITAR";
+
+    }, error => { console.log(error) });
+
+  }
+
+  // Actualizar el tipo de documento 
+  public ActualizarTipDoc() {
+
+    var nuevotipdoc = this.ActualizarATipDoc.getRawValue()['textnuevotipdoc'];
+    var nuevoinitipdoc = this.ActualizarATipDoc.getRawValue()['textnuevoinicialestipdoc'];
+
+    var cadena = { "id_tip_doc": this.BuscarEvalor, "tipo_documento": nuevotipdoc, "iniciales_tip_doc": nuevoinitipdoc };
+
+    this.servi.updateTipDoc(cadena).then
+      (
+        res => {
+          console.log("res  ", res)
+        }
+      ).catch(err => {
+        console.log(err)
+      });
+
+    //this.BuscarEvalor = 0;
+    this.ActualizarATipDoc.reset();
+
+  }
 
   ngOnInit(): void {
+
     this.ListaTiposDocum = this.formBuilder.group(
       {
 
